c695a8723c1daabb59d1a7fb195b844066802614
mskok <mskok@vendavo.com>
2023-02-12 22:35:00 +0100

    added modal for displaying Merchant (Shop) details

---
+++ Data/ShopDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace OrdersReaderBlazorApp.Data
+{
+    public class ShopDetails
+    {
+        public string merchantID { get; set; }
+        public string chain { get; set; }
+        public string name { get; set; }
+        public string streetAddress { get; set; }
+        public string town { get; set; }
+        public string postCode { get; set; }
+        public string phone1 { get; set; }
+        public string phone2 { get; set; }
+        public string emails { get; set; }
+        public string contact { get; set; }
+        public string shopFreeText { get; set; }
+        public string mcc { get; set; }
+        public string action { get; set; }
+    }
+}
+
