98da673eed4de616e94600887ab41fbd9be0d5e9
Matej Skok <matej.skok@icloud.com>
2023-07-20 08:20:06 +0200

    terminalFreeText popup

---
+++ Data/TerminalFreeTextDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace OrdersReaderBlazorApp.Data
+{
+    public class TerminalFreeTextDetail
+    {
+        public string terminalId { get; set; }
+        public string terminalFreeText { get; set; } // most important property here
+        public string terminalCompanyId { get; set; }
+        public string terminalShopId { get; set; }
+        public string action { get; set; }
+
+        public string backgroundPopupColorCssClass { get; set; }
+    }
+}
+
---
+++ wwwroot/TerminalsGrid.js
@@ -0,0 +1,35 @@
+﻿function changeRowColor() {
+    let greenElements = document.querySelectorAll('span.green');
+    if (greenElements) {
+        greenElements.forEach((element) => {
+            let parent = element.parentNode.parentNode;
+            parent.classList.add('bg-green');
+            parent.classList.add('text-dark');
+            parent.classList.add('bg-opacity-75');
+        });
+    }
+
+    let redElements = document.querySelectorAll('span.red');
+    redElements.forEach((element) => {
+        let parent = element.parentNode.parentNode;
+        parent.classList.add('bg-red');
+        parent.classList.add('text-dark');
+        parent.classList.add('bg-opacity-75');
+    });
+
+    let orangeElements = document.querySelectorAll('span.orange');
+    orangeElements.forEach((element) => {
+        let parent = element.parentNode.parentNode;
+        parent.classList.add('bg-orange');
+        parent.classList.add('text-dark');
+        parent.classList.add('bg-opacity-75');
+    });
+
+    let grayElements = document.querySelectorAll('span.gray');
+    grayElements.forEach((element) => {
+        let parent = element.parentNode.parentNode;
+        parent.classList.add('bg-gray');
+        parent.classList.add('text-dark');
+        parent.classList.add('bg-opacity-75');
+    });
+}
